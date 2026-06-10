a1e0ba2b71bdc33411e088dee3f740e1be7995e9
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-10 03:40:18 -0400

    implementacion de metodos para el login sin usar google

--- frontend/src/features/auth/api/auth.api.ts
+++ frontend/src/features/auth/api/auth.api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { LoginCredentials, RegisterCredentials } from '../types/auth.types';
 
 export const authApi = {
   loginWithGoogle: async (idToken: string) => {
@@ -6,5 +7,16 @@ export const authApi = {
       idTokenGoogle: idToken
     });
     return response.data.token;
-  }
+  },
+    // TODO: ajustar endpoint cuando el backend esté listo
+  loginWithCredentials: async (credentials: LoginCredentials) => {
+    const response = await axios.post('http://localhost:5198/api/iam/auth/login', credentials);
+    return response.data.token;
+  },
+ 
+  // TODO: ajustar endpoint cuando el backend esté listo
+  register: async (credentials: RegisterCredentials) => {
+    const response = await axios.post('http://localhost:5198/api/iam/auth/register', credentials);
+    return response.data.token;
+  },
 };
