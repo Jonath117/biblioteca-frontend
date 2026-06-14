4f514f98156ebacc45c32e8e0a35a07cc4909713
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-14 14:33:49 -0400

    se cambio el nombre de los types para que haya consistencia con el backend

--- frontend/src/features/auth/types/auth.types.ts
+++ frontend/src/features/auth/types/auth.types.ts
@@ -2,10 +2,10 @@ export interface LoginCredentials {
   email: string;
   password: string;
 }
- 
+
 export interface RegisterCredentials {
-  firstName: string;
-  lastName: string;
+  nombre: string;
+  apellido: string;
   email: string;
   password: string;
 }
