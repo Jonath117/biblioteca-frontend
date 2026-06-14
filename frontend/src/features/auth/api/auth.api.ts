cd0b05ce0dac10f042f8bb79bdf37549a6ebd0c2
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-14 14:30:25 -0400

    se oculto la url mediante el .env y se cambio el tipo de dato que devuelve el metodo register

--- frontend/src/features/auth/api/auth.api.ts
+++ frontend/src/features/auth/api/auth.api.ts
@@ -1,20 +1,22 @@
 import axios from 'axios';
 import type { LoginCredentials, RegisterCredentials } from '../types/auth.types';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const authApi = {
   loginWithGoogle: async (idToken: string) => {
-    const response = await axios.post('http://localhost:5198/api/iam/auth/sso', {
+    const response = await axios.post(`${API_URL}/api/iam/auth/sso`, {
       idTokenGoogle: idToken
     });
     return response.data.token;
   },
   loginWithCredentials: async (credentials: LoginCredentials) => {
-    const response = await axios.post('http://localhost:5198/api/iam/auth/login', credentials);
+    const response = await axios.post(`${API_URL}/api/iam/auth/login`, credentials);
     return response.data.token;
   },
  
   register: async (credentials: RegisterCredentials) => {
-    const response = await axios.post('http://localhost:5198/api/iam/auth/register', credentials);
-    return response.data.token;
+    const response = await axios.post(`${API_URL}/api/iam/auth/registro`, credentials);
+    return response.data.id;
   },
 };
