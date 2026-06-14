e5dc8fc8fdebab22bb04b859312451a821852f2f
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-14 14:35:33 -0400

    se modifico para que pueda retornar las exceptions del backend

--- frontend/src/features/auth/hooks/useRegister.ts
+++ frontend/src/features/auth/hooks/useRegister.ts
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '../../../store/authStore';
+//import { useAuthStore } from '../../../store/authStore';
 import { authApi } from '../api/auth.api';
 import type { RegisterCredentials } from '../types/auth.types';
 
+import axios from 'axios'; 
+
 export const useRegister = () => {
   const navigate = useNavigate();
-  const { login } = useAuthStore();
+  //const { login } = useAuthStore();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -15,12 +17,22 @@ export const useRegister = () => {
       setIsLoading(true);
       setError(null);
 
-      const token = await authApi.register(credentials);
-      login(token);
-      navigate('/dashboard');
+      await authApi.register(credentials);
+
+      alert("¡Cuenta creada con éxito! Por favor, inicia sesión con tus credenciales.");
+
+      navigate('/login');
     } catch (err) {
-      setError('No se pudo crear la cuenta. Verifica los datos e intenta de nuevo.');
       console.error('Error al registrarse:', err);
+
+      if (axios.isAxiosError(err) && err.response?.data?.message) {
+        setError(err.response.data.message);
+      } else if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Ocurrió un error inesperado. Por favor, intenta de nuevo.');
+      }
+
     } finally {
       setIsLoading(false);
     }
