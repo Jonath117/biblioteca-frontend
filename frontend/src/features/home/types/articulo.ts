a6fa12b042ad3280098046166fda4b1c70a01466
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-18 14:41:04 -0400

    implementacion del componente encargado de traer los articulos publicados

---
+++ frontend/src/features/home/types/articulo.ts
@@ -0,0 +1,11 @@
+export interface ArticuloPublicado {
+    id: string;
+    titulo: string;
+    nombresAutores: string;
+    resumen: string;
+    archivoUrl: string;
+    carrera: string;
+    materia: string;
+    fechaPublicacion: string;
+    etiquetas: string[];
+}
